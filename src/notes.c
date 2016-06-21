90106d8c162b39d5110c44345d7a336a42378b8f
hexaguin <hexaguin@gmail.com>
2016-06-20 19:10:33 -0600

    More indent stuff?

--- src/notes.c
+++ src/notes.c
@@ -38,42 +38,42 @@ void note_data_received(DictionaryIterator* iterator) {
 }
 
 static void note_window_load(Window *window) {
-  //TODO populate this with all the windowy stuff currently in note_init
+	//TODO populate this with all the windowy stuff currently in note_init
 }
 
 static void note_window_unload(Window *window) {
-  displayingNote = false;
+	displayingNote = false;
 }
 
-void note_init() { 
+void note_init() {
 	fullWindow = window_create();
 	Layer* topLayer = window_get_root_layer(fullWindow);
-  GRect windowBounds = layer_get_bounds(topLayer);
-  
-  window_set_window_handlers(fullWindow, (WindowHandlers) {
-    .load = note_window_load,
-    .unload = note_window_unload
-  });
-  
-  s_statusbar_layer = status_bar_layer_create();
-  status_bar_layer_set_colors(s_statusbar_layer, COLOR_HIGHLIGHT, COLOR_FOREGROUND);
-  status_bar_layer_set_separator_mode(s_statusbar_layer, StatusBarLayerSeparatorModeDotted);
-  
-  windowBounds.origin.y+=STATUS_BAR_LAYER_HEIGHT; //resize "usable" area to not overlap with statusbar
-  windowBounds.size.h-=STATUS_BAR_LAYER_HEIGHT;
+	GRect windowBounds = layer_get_bounds(topLayer);
+
+	window_set_window_handlers(fullWindow, (WindowHandlers) {
+		.load = note_window_load,
+		.unload = note_window_unload
+	});
+
+	s_statusbar_layer = status_bar_layer_create();
+	status_bar_layer_set_colors(s_statusbar_layer, COLOR_HIGHLIGHT, COLOR_FOREGROUND);
+	status_bar_layer_set_separator_mode(s_statusbar_layer, StatusBarLayerSeparatorModeDotted);
+
+	windowBounds.origin.y+=STATUS_BAR_LAYER_HEIGHT; //resize "usable" area to not overlap with statusbar
+	windowBounds.size.h-=STATUS_BAR_LAYER_HEIGHT;
 
 	fullNoteText = text_layer_create(windowBounds);
 	text_layer_set_font(fullNoteText, fonts_get_system_font(FONT_KEY_GOTHIC_18));
 
 	scroller = scroll_layer_create(windowBounds);
-  scroll_layer_set_shadow_hidden(scroller, true);
+	scroll_layer_set_shadow_hidden(scroller, true);
 	scroll_layer_set_click_config_onto_window(scroller, fullWindow);
 	scroll_layer_add_child(scroller, (Layer *) fullNoteText);
 
 	for (uint16_t i = 0; i < 3000; i++)
 		fullNote[i] = 0;
 
-  layer_add_child(topLayer, status_bar_layer_get_layer(s_statusbar_layer));
+	layer_add_child(topLayer, status_bar_layer_get_layer(s_statusbar_layer));
 	layer_add_child(topLayer, (Layer *) scroller);
 
 	window_stack_push(fullWindow, true /* Animated */);
--- src/popup.c
+++ src/popup.c
@@ -12,73 +12,73 @@ static GBitmap *s_icon_bitmap;
 char *popup_message = "Even the error message broke."; //Gets replaced when the window is made
 
 static void icon_update_proc(Layer *layer, GContext *ctx) {
-  GRect bounds = layer_get_bounds(layer);
-  GRect bitmap_bounds = gbitmap_get_bounds(s_icon_bitmap);
-  graphics_context_set_compositing_mode(ctx, GCompOpSet);
-  graphics_draw_bitmap_in_rect(ctx, s_icon_bitmap, (GRect){.origin = bounds.origin, .size = bitmap_bounds.size});
+	GRect bounds = layer_get_bounds(layer);
+	GRect bitmap_bounds = gbitmap_get_bounds(s_icon_bitmap);
+	graphics_context_set_compositing_mode(ctx, GCompOpSet);
+	graphics_draw_bitmap_in_rect(ctx, s_icon_bitmap, (GRect){.origin = bounds.origin, .size = bitmap_bounds.size});
 }
 
 static void background_update_proc(Layer *layer, GContext *ctx) {
-  graphics_context_set_fill_color(ctx, PBL_IF_COLOR_ELSE(GColorYellow, GColorWhite));
-  graphics_fill_rect(ctx, layer_get_bounds(layer), 0, 0);
+	graphics_context_set_fill_color(ctx, PBL_IF_COLOR_ELSE(GColorYellow, GColorWhite));
+	graphics_fill_rect(ctx, layer_get_bounds(layer), 0, 0);
 }
 
 static void window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
+	Layer *window_layer = window_get_root_layer(window);
+	GRect bounds = layer_get_bounds(window_layer);
 
-  s_background_layer = layer_create(bounds);
-  layer_set_update_proc(s_background_layer, background_update_proc);
-  layer_add_child(window_layer, s_background_layer);
+	s_background_layer = layer_create(bounds);
+	layer_set_update_proc(s_background_layer, background_update_proc);
+	layer_add_child(window_layer, s_background_layer);
 
-  s_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_WARNING);
-  GRect bitmap_bounds = gbitmap_get_bounds(s_icon_bitmap);
+	s_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_WARNING);
+	GRect bitmap_bounds = gbitmap_get_bounds(s_icon_bitmap);
 
-  s_icon_layer = layer_create(
-      GRect(
-        (bounds.size.w/2)-(bitmap_bounds.size.w/2), 
-        POPUP_WINDOW_MARGIN, 
-        bitmap_bounds.size.w, 
-        bitmap_bounds.size.h
-      )
-  );
-  layer_set_update_proc(s_icon_layer, icon_update_proc);
-  layer_add_child(window_layer, s_icon_layer);
+	s_icon_layer = layer_create(
+		GRect(
+			(bounds.size.w/2)-(bitmap_bounds.size.w/2),
+			POPUP_WINDOW_MARGIN,
+			bitmap_bounds.size.w,
+			bitmap_bounds.size.h
+		)
+	);
+	layer_set_update_proc(s_icon_layer, icon_update_proc);
+	layer_add_child(window_layer, s_icon_layer);
 
-  s_label_layer = text_layer_create(GRect(
-      POPUP_WINDOW_MARGIN,
-      POPUP_WINDOW_MARGIN*2 + bitmap_bounds.size.h,
-      bounds.size.w - (2 * POPUP_WINDOW_MARGIN),
-      bounds.size.h
-      ));
-  text_layer_set_text(s_label_layer, popup_message);
-  text_layer_set_background_color(s_label_layer, GColorClear);
-  text_layer_set_text_alignment(s_label_layer, PBL_IF_ROUND_ELSE(GTextAlignmentCenter, GTextAlignmentLeft));
-  text_layer_set_font(s_label_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
-  layer_add_child(window_layer, text_layer_get_layer(s_label_layer));
+	s_label_layer = text_layer_create(GRect(
+		POPUP_WINDOW_MARGIN,
+		POPUP_WINDOW_MARGIN*2 + bitmap_bounds.size.h,
+		bounds.size.w - (2 * POPUP_WINDOW_MARGIN),
+		bounds.size.h
+		));
+	text_layer_set_text(s_label_layer, popup_message);
+	text_layer_set_background_color(s_label_layer, GColorClear);
+	text_layer_set_text_alignment(s_label_layer, PBL_IF_ROUND_ELSE(GTextAlignmentCenter, GTextAlignmentLeft));
+	text_layer_set_font(s_label_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+	layer_add_child(window_layer, text_layer_get_layer(s_label_layer));
 }
 
 static void window_unload(Window *window) {
-  layer_destroy(s_background_layer);
-  layer_destroy(s_icon_layer);
+	layer_destroy(s_background_layer);
+	layer_destroy(s_icon_layer);
 
-  text_layer_destroy(s_label_layer);
+	text_layer_destroy(s_label_layer);
 
-  gbitmap_destroy(s_icon_bitmap);
+	gbitmap_destroy(s_icon_bitmap);
 
-  window_destroy(window);
-  s_main_window = NULL;
+	window_destroy(window);
+	s_main_window = NULL;
 }
 
 void popup_window_push(char *message) {//TODO add different types (e.g. error, info, confirmation, ect.)
-  popup_message = message;
-  if(!s_main_window) {
-    s_main_window = window_create();
-    window_set_background_color(s_main_window, GColorBlack);
-    window_set_window_handlers(s_main_window, (WindowHandlers) {
-        .load = window_load,
-        .unload = window_unload
-    });
-  }
-  window_stack_push(s_main_window, true);
+	popup_message = message;
+	if(!s_main_window) {
+	 s_main_window = window_create();
+	 window_set_background_color(s_main_window, GColorBlack);
+	 window_set_window_handlers(s_main_window, (WindowHandlers) {
+			.load = window_load,
+			.unload = window_unload
+	 });
+	}
+	window_stack_push(s_main_window, true);
 }
